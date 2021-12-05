a107d0da4a5187395a38ac842e4a5252a61b2cd8
Vikulov Andrey <vikulov.prog@gmail.com>
2021-12-06 01:56:21 +0400

    1

---
+++ src/containers/whatCPT3/WhatCPT3.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import './whatCPT3.css';
+const WhatCPT3 = () => {
+    return (
+        <div>
+            WhatCPT3
+        </div>
+    )
+}
+
+export default WhatCPT3
